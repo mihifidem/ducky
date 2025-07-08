20c33b2ada52845964cff43b65e3257e206fb605
Stepan <sd.steps512@gmail.com>
2025-07-08 13:38:17 +0200

    forum_nav_bar

--- config/urls.py
+++ config/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('', core_views.home, name='home'),  # Página de inicio
     path('account/', include('django.contrib.auth.urls')),  # login/logout
+    path('forum/', include('forum.urls')),  # URLs del foro
 
 
 ]
--- forum/urls.py
+++ forum/urls.py
@@ -2,13 +2,13 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.forum_home, name='forum_home'),
-    path('pregunta/<int:pk>/', views.pregunta_detail, name='pregunta_detail'),
-    path('pregunta/create_private/', views.pregunta_create, name='pregunta_create_private'),
-    path('pregunta/create_public/', views.pregunta_create_public, name='pregunta_create_public'),
-    path('pregunta/<int:pk>/edit/', views.pregunta_edit, name='pregunta_edit'),
-    path('pregunta/<int:pk>/delete/', views.pregunta_delete, name='pregunta_delete'),
-    path('respuesta/create/<int:pk>/', views.respuesta_create, name='respuesta_create'),
-    path('respuesta/<int:pk>/edit/', views.respuesta_edit, name='respuesta_edit'),
-    path('respuesta/<int:pk>/delete/', views.respuesta_delete, name='respuesta_delete'),
+    path('forum/', views.forum_home, name='forum_home'),
+    path('forum/pregunta/<int:pk>/', views.pregunta_detail, name='pregunta_detail'),
+    path('forum/pregunta/create_private/', views.pregunta_create_private, name='pregunta_create_private'),
+    path('forum/pregunta/create_public/', views.pregunta_create_public, name='pregunta_create_public'),
+    path('forum/pregunta/<int:pk>/edit/', views.pregunta_edit, name='pregunta_edit'),
+    path('forum/pregunta/<int:pk>/delete/', views.pregunta_delete, name='pregunta_delete'),
+    path('forum/respuesta/create/<int:pk>/', views.respuesta_create, name='respuesta_create'),
+    path('forum/respuesta/<int:pk>/edit/', views.respuesta_edit, name='respuesta_edit'),
+    path('forum/respuesta/<int:pk>/delete/', views.respuesta_delete, name='respuesta_delete'),
 ]
