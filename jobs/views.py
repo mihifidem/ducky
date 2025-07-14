6b528807c07db60a306080a5a68dd4dbd536b63a
anaaalonso <ana.alonsorosado@gmail.com>
2025-07-14 12:11:37 +0200

    prueba 3461289592510

--- jobs/views.py
+++ jobs/views.py
@@ -42,7 +42,7 @@ def job_offer_list(request):
     })
    
     
-=======
+
     offers = JobOffer.objects.all() # O tu lógica para obtener las ofertas
 
 #     # Agrega esta lógica para verificar si el usuario es un headhunter
@@ -58,7 +58,7 @@ def job_offer_list(request):
 # def job_offer_list(request):
 #     offers = JobOffer.objects.filter(is_active=True).order_by('-created_at')
 #     return render(request, 'jobs/job_offer_list.html', {'offers': offers})
->>>>>>> 54909d7fb482761a61441433bb297c1b25098337
+
 
 
 def job_offer_detail(request, pk):
