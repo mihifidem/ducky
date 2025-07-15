b3fc291dbdf4f52990471affc4ca0beeea3203a8
Marc <marc.quimis@gmail.com>
2025-07-15 13:30:38 +0100

    forms completed

--- forum/forms.py
+++ forum/forms.py
@@ -5,6 +5,11 @@ class PreguntaFormPublic(forms.ModelForm):
     class Meta:
         model = Pregunta
         fields = ['titulo', 'pregunta', 'sector']
+        widgets = {
+            'sector': forms.Select(attrs={'class': 'form-control'}),
+            'pregunta': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 
 class PreguntaFormPrivate(forms.ModelForm):
