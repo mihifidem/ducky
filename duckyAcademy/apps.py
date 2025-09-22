08fe9dd2651097f78411bc467cb986761f2435e5
Eloy <eloydominguez3@gmail.com>
2025-09-22 12:25:51 +0200

    LImpiando_rama

--- duckyAcademy/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DuckyacademyConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "duckyAcademy"
--- management_test/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-class Test(models.Model):
-    nombre = models.CharField(max_length=100)
-    creado = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.nombre
