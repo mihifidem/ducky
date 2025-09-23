75d4ead721aa32962385cb189a9635a9bce201eb
Eloy <eloydominguez3@gmail.com>
2025-09-23 21:59:21 +0200

    Error mio lo siento

--- management_test/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import ComentariosProfessores
-
-class ComentarioForm(forms.ModelForm):
-    class Meta:
-        model = ComentariosProfessores
-        fields = ['comentario']
-
