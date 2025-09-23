1f18cbeadba68b2bff2401bd51bce1492dead62b
Eloy <eloydominguez3@gmail.com>
2025-09-23 21:43:02 +0200

    1_Limpiando Rama_23/09/2025 21:42_Eloy

--- management_test/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ResultadoTest, PreguntaRespondida, ResultadoInteligencia, ComentariosProfessores
-
-admin.site.register(ResultadoTest)
-admin.site.register(PreguntaRespondida)
-admin.site.register(ResultadoInteligencia)
-admin.site.register(ComentariosProfessores)
