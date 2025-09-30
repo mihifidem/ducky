d163ff9aea0be1450e2465f2b2302f01b910b55a
Jordi-bit <bitcurso3@gmail.com>
2025-09-30 15:49:02 +0200

    T1-T14_Actualizado

---
+++ account/cv_manager/languages.py
@@ -0,0 +1,30 @@
+
+# abrir terminal y ejecutar python manage.py shell   python manage.py shell
+# copiar el script en la shell y ejecutar
+# salir de la shell con exit
+# apareceran en la tabla languages los idiomas.
+
+
+
+
+from account.cv_manager.models import Language
+
+# Lista de idiomas comunes
+idiomas = [
+    "Inglés",
+    "Español",
+    "Francés",
+    "Alemán",
+    "Italiano",
+    "Portugués",
+    "Chino",
+    "Japonés",
+    "Ruso",
+    "Árabe"
+]
+
+# Crear los idiomas si no existen
+for nombre in idiomas:
+    Language.objects.get_or_create(name=nombre)
+
+print("✅ Idiomas inicializados correctamente.")
