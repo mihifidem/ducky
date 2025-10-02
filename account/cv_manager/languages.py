9c1d58394e646da86591324e5016e98d31ac8273
jordi <bitcurso3@gmail.com>
2025-10-02 10:09:02 +0200

    toda_app_t1-t14.

--- account/cv_manager/languages.py
+++ account/cv_manager/languages.py
@@ -3,11 +3,6 @@
 # copiar el script en la shell y ejecutar
 # salir de la shell con exit
 # apareceran en la tabla languages los idiomas.
-# abrir terminal y ejecutar python manage.py shell   python manage.py shell
-# copiar el script en la shell y ejecutar
-# salir de la shell con exit
-# apareceran en la tabla languages los idiomas.
-
 
 
 from account.cv_manager.models import Language
